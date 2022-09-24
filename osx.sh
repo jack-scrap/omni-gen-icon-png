9ca4cfd8507d82d7f886294dbcb3b02996a44eea
Jack <jackhasakeyboard@gmail.com>
2022-09-23 19:10:39 -0600

    Properly updated scripts to reflect newly named executable

--- osx.sh
+++ osx.sh
@@ -3,5 +3,5 @@
 SZ=(16 32 128 256 512)
 
 for s in "${SZ[@]}"; do
-	./omni_gen_icon $s;
+	./omni_gen_icon_png $s;
 done
--- win.sh
+++ win.sh
@@ -3,5 +3,5 @@
 SZ=(16 24 32 40 48 64 96 128 256)
 
 for s in "${SZ[@]}"; do
-	./omni_gen_icon $s;
+	./omni_gen_icon_png $s;
 done
